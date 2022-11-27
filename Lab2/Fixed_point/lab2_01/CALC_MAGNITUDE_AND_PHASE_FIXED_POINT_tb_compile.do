ca4202bae7eade727a44d93bcaea7ead5af92154
illiaGxx <115420741+illiaGxx@users.noreply.github.com>
2022-11-28 00:15:24 +0200

    Add files via upload

---
+++ Lab2/Fixed_point/lab2_01/CALC_MAGNITUDE_AND_PHASE_FIXED_POINT_tb_compile.do
@@ -0,0 +1,5 @@
+vlib work
+vlog  atan2_cordic_nw.v
+vlog  Sqrt.v
+vlog  CALC_MAGNITUDE_AND_PHASE_FIXED_POINT.v
+vlog  CALC_MAGNITUDE_AND_PHASE_FIXED_POINT_tb.v
---
+++ Lab2/Fixed_point/lab2_01/clearhighlighting.m
@@ -0,0 +1,6 @@
+SLStudio.Utils.RemoveHighlighting(get_param('lab2_01', 'handle'));
+SLStudio.Utils.RemoveHighlighting(get_param('gm_lab2_01', 'handle'));
+annotate_port('gm_lab2_01/CALC_MAGNITUDE_AND_PHASE_FIXED_POINT/Add', 0, 1, '');
+annotate_port('gm_lab2_01/CALC_MAGNITUDE_AND_PHASE_FIXED_POINT/Complex to Real-Imag1', 0, 1, '');
+annotate_port('gm_lab2_01/CALC_MAGNITUDE_AND_PHASE_FIXED_POINT/Product', 0, 1, '');
+annotate_port('gm_lab2_01/CALC_MAGNITUDE_AND_PHASE_FIXED_POINT/Sqrt', 1, 1, '');
